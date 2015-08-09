fbc0ff2e40afaf5e167e097174d4f54fad8c6d92
Josh Cummings <joshcummings@gmx.com>
2015-08-09 11:39:38 +1000

    dotsymlinks.sh - remove backup

--- dotsymlinks.sh
+++ dotsymlinks.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 
 dir=~/.dotfiles
-backup_dir=~/.dotfiles.bak
 files="bashrc bash_aliases bash_profile inputrc screenrc urxvt vimrc vim xmodmaprc xsessionrc Xresources"
 
-mkdir -p $backup_dir
-
 for file in $files; do
-	mv ~/.$file $backup_dir
 	ln -s $dir/$file ~/.$file
 done
